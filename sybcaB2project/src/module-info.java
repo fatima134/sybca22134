9c78cc7191b47500af93ef9a821e193d59264406
fatimaCasetools <fatima@rosarycollege.org>
2024-01-08 09:47:13 +0530

    first github commit

---
+++ sybcaB2project/src/module-info.java
@@ -0,0 +1,2 @@
+module sybcaB2project {
+}
---
+++ sybcaB2project/src/sybcaB2project/clsGithub.java
@@ -0,0 +1,13 @@
+package sybcaB2project;
+
+public class clsGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+		
+
+	}
+
+}
