0d1aa550d0f3ffe3fec3343a7e602784b0e0f232
fatimamakandar <rbca22134.rcca@gmail.com>
2024-01-15 11:04:44 +0530

    commit

--- fatimamakandar/src/fatimamakandar/add2nos.java
+++ fatimamakandar/src/fatimamakandar/add2nos.java
@@ -5,11 +5,15 @@ public class add2nos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=2;
-		int b=4;
-		int add;
+		int a=10;
+		int b=5;
+		int add,sub;
 		add=a+b;
 		System.out.println("the addition of the number are:"+add);
+		
+		
+		sub=a-b;
+		System.out.println("the addition of the number are:"+sub);
 		
 	
 		
